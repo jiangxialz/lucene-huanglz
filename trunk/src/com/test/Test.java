5a0088156991375355fc240a93618e0d5f244030
huanglz19871030@gmail.com <huanglz19871030@gmail.com@28d25f9b-c7cd-db60-4da3-ac8969450f57>
2013-04-27 08:58:13 +0000

    增加相关测试信息

--- trunk/src/com/test/Test.java
+++ trunk/src/com/test/Test.java
@@ -5,7 +5,8 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
+
+import org.apache.commons.lang.StringUtils;
 
 public class Test 
 {
@@ -54,6 +55,7 @@ public class Test
 		
 	}*/
 	
+	@SuppressWarnings("unchecked")
 	public static void main(String[] args) {
 //		String aString = "adfadsfa该罚的，http:www.aceona.com ";
 //		String bString = "adfadsfa该罚的，<a href=\"http://www.aceona.com\">";
@@ -67,16 +69,72 @@ public class Test
 //			System.out.println(aString);
 //		}
 		
-		int maxValue = Integer.MIN_VALUE;
-		Map<String, Integer> map = new HashMap<String, Integer>();
-		map.put("2000", 10);
-		map.put("2000", 15);
-		map.put("2000", 20);
-		Iterator<Integer> values = map.values().iterator();
-		while (values.hasNext()) {
-			maxValue = Math.max(maxValue, values.next().intValue());
+		List<Test> list =  new ArrayList<Test>();
+		Test test1 = new Test();
+		test1.setAttr_id("品牌");
+		test1.setValue("test1");
+		
+		Test test2 = new Test();
+		test2.setAttr_id("品牌");
+		test2.setValue("test2");
+		
+		Test test3 = new Test();
+		test3.setAttr_id("品牌");
+		test3.setValue("test3");
+		
+		Test test4 = new Test();
+		test4.setAttr_id("材质");
+		test4.setValue("不知道");
+		
+//		list.add(test1);
+//		list.add(test2);
+//		list.add(test3);
+//		list.add(test4);
+		
+		Map<String, String> labelValueMap = new HashMap<String, String>();
+		
+		for (int i = 0; i < list.size(); i++) {
+			Test test = list.get(i);
+			// 如果map中已经存在key，则将value值加进数组中
+			String label = test.getAttr_id();
+			String value = test.getValue();
+			if(labelValueMap.containsKey(label)){
+				String valueStr =  labelValueMap.get(label);
+				labelValueMap.put(label, valueStr + "," +  value);
+			}else{ // 如果map中不包含key，则将记录直接加进数组中
+				labelValueMap.put(label, value);
+			}
 		}
-		System.out.println(maxValue);
+		
+		StringBuffer sbBuffer = new StringBuffer();
+		for (Iterator<Map.Entry<String, String>> it = labelValueMap.entrySet().iterator(); it.hasNext();)
+        {
+            Map.Entry<String, String> entry = it.next();
+            sbBuffer.append(entry.getKey()).append(":").append(entry.getValue()).append("|");
+        }
+		if(StringUtils.isNotBlank(sbBuffer.toString())){
+			System.err.println( sbBuffer.substring(0, sbBuffer.length() - 1));
+		}
+		String resultStr = sbBuffer.toString();
+		resultStr = StringUtils.isNotBlank(sbBuffer.toString()) ? resultStr.substring(0, resultStr.length() - 1) : "";
+		System.err.println("resultStr======" + resultStr);
+		
 	}
+	
+	private String attr_id;
+	private String value;
+	public String getAttr_id() {
+		return attr_id;
+	}
+	public void setAttr_id(String attr_id) {
+		this.attr_id = attr_id;
+	}
+	public String getValue() {
+		return value;
+	}
+	public void setValue(String value) {
+		this.value = value;
+	}
+	
 
 }
